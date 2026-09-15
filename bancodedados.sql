0f6be6bdfbce16a605bfa88fe522503dd0e18a2b
Brandow Figueira <brandow.cgrj@hotmail.com>
2026-09-15 14:26:53 -0300

    Arquivo do banco de dados com a view do relatório

--- bancodedados.sql
+++ bancodedados.sql
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS `assunto` (
   `codAs` int NOT NULL AUTO_INCREMENT,
   `Descricao` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`codAs`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Exportação de dados foi desmarcado.
 
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS `autor` (
   `CodAu` int NOT NULL AUTO_INCREMENT,
   `Nome` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`CodAu`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Exportação de dados foi desmarcado.
 
@@ -46,7 +46,7 @@ CREATE TABLE IF NOT EXISTS `livro` (
   `AnoPublicacao` varchar(4) COLLATE utf8mb4_unicode_ci NOT NULL,
   `valor` decimal(10,2) NOT NULL,
   PRIMARY KEY (`Codl`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Exportação de dados foi desmarcado.
 
@@ -75,6 +75,23 @@ CREATE TABLE IF NOT EXISTS `livro_autor` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para view booksystem.vw_relatorio_livros
+-- Criando tabela temporária para evitar erros de dependência de VIEW
+CREATE TABLE `vw_relatorio_livros` (
+	`Codl` INT NOT NULL,
+	`Titulo` VARCHAR(1) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`Editora` VARCHAR(1) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`Edicao` INT NOT NULL,
+	`AnoPublicacao` VARCHAR(1) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`Valor` DECIMAL(10,2) NOT NULL,
+	`Autores` TEXT NULL COLLATE 'utf8mb4_unicode_ci',
+	`Assuntos` TEXT NULL COLLATE 'utf8mb4_unicode_ci'
+) ENGINE=MyISAM;
+
+-- Removendo tabela temporária e criando a estrutura VIEW final
+DROP TABLE IF EXISTS `vw_relatorio_livros`;
+CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `vw_relatorio_livros` AS select `l`.`Codl` AS `Codl`,`l`.`Titulo` AS `Titulo`,`l`.`Editora` AS `Editora`,`l`.`Edicao` AS `Edicao`,`l`.`AnoPublicacao` AS `AnoPublicacao`,`l`.`valor` AS `Valor`,group_concat(distinct `a`.`Nome` separator ', ') AS `Autores`,group_concat(distinct `s`.`Descricao` separator ', ') AS `Assuntos` from ((((`livro` `l` left join `livro_autor` `la` on((`l`.`Codl` = `la`.`Livro_Codl`))) left join `autor` `a` on((`la`.`Autor_CodAu` = `a`.`CodAu`))) left join `livro_assunto` `las` on((`l`.`Codl` = `las`.`Livro_Codl`))) left join `assunto` `s` on((`las`.`Assunto_codAs` = `s`.`codAs`))) group by `l`.`Codl`,`l`.`Titulo`,`l`.`Editora`,`l`.`Edicao`,`l`.`AnoPublicacao`,`l`.`valor` order by `l`.`Titulo`;
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
